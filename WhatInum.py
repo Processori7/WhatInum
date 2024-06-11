a57de06991bd3f2f540d4d00a46ddc00702497a7
Igor Usenko <usenkoigor01@gmail.com>
2024-06-11 18:20:52 +0300

    Готово

---
+++ WhatInum.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = keras.datasets.mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+  keras.layers.Flatten(input_shape=(28, 28)),
+  keras.layers.Dense(128, activation='relu'),
+  keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+# Распознанные классы
+predicted_classes = np.argmax(predictions, axis=1)
+
+# Вывод результата
+print("Предсказанная метка: ", predicted_classes[1])
+plt.imshow(test_images[1], cmap=plt.cm.binary)
+plt.show()
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Описание
+Это небольшой проект, который позволяет распознавать число на картинке.
+# Использование
+Клонировать репозиторий:
+```
+git clone https://github.com/Processori7/WhatInum.git```
+Перейти в каталог:
+```
+cd Emotion_recognition
+```
+Создать виртуальное окружение:
+```
+python -m venv venv
+```
+Активировать окружение:
+```
+venv\Scripts\activate.bat
+```
+или
+```
+.venv\Scripts\activate.bat
+```
+Выполнить установку зависимостей:
+```
+pip install -r requirements.txt
+```
+Далее запустить файл:
+```
+python WhatInum.py
+```
